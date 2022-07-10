15552e8df4de631f69af4d971cbddc1e5c55851d
aluvium <aluvium@protonmail.com>
2022-07-10 18:17:48 +0000

    adding possibility ot change size of dice and times you roll

--- dice_roller.py
+++ dice_roller.py
@@ -1,13 +1,26 @@
 import random
+# crit fail
+
+def verbose(arg):
+    if arg == 1:
+        print(f' You did roll {arg} - Critical fail!')
+    elif arg == 6:
+        print(f' You did roll {arg} - Critical hit!')
+    else:  
+        print(f'You rolled a {arg}')
+    return 0
+
 
 def main():
-    dice_rolls=2
+    dice_rolls=int(input(' How many dice would like to roll?  '))
+    dice_size=int(input(' How many sides are the dice  '))
     dice_sum=0
 
     for each in range(0,dice_rolls):
-        roll=random.randint(1,6)
+        roll=random.randint(1,dice_size)
         dice_sum+=roll
-        print(f'You rolled a {roll}')
+        verbose(roll)
+        
     print(f'Your total roll is {dice_sum}')
 
 if __name__== "__main__":
